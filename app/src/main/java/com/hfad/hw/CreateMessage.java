8115f4fd1e19bc8bae37d4204a05c778097ac1c3
adriant101 <42621872+adriant101@users.noreply.github.com>
2019-02-05 17:06:21 -0700

    Initial commit

---
+++ app/src/main/java/com/hfad/hw/CreateMessage.java
@@ -0,0 +1,13 @@
+package com.hfad.hw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
